deaa5b75b3b81083662273fd55564b5037bb6bfa
Dipak Khedkar <113326485+dipakkhedkar1104@users.noreply.github.com>
2024-02-17 18:44:20 +0530

    Delete My_FullStack_Project_SpringB-react directory

--- My_FullStack_Project_SpringB-react/My_FullStack_Project_SpringB-react/fullstack-backend/src/main/java/com/myproject/fullstackbackend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myproject.fullstackbackend.repository;
-
-import com.myproject.fullstackbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	
-
-}
